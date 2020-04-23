41dff846f41209d11e8f758ba2bbece6a6a1e3f0
Yalda Ali Aghazadeh <zahraaliaghazadeh@Yaldas-MacBook-Pro.local>
2020-04-23 00:34:11 -0700

    basic file structures made based on readme

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ("philly cheese steak");
+INSERT INTO burgers (burger_name) VALUES ('double cheeseburger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('chicken delux', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('mushroom burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('beyond burger', true);
+INSERT INTO burgers (burger_name) VALUES ('chicken delux spicy');
