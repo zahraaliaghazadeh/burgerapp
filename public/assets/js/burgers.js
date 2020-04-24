c3f4ed8b756c43d5e93f4319a45ffaefa968c870
Yalda Ali Aghazadeh <zahraaliaghazadeh@Yaldas-MacBook-Pro.local>
2020-04-23 22:01:40 -0700

    fixed the problem with the radio buttons

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -26,8 +26,8 @@ $(function() {
       event.preventDefault();
   
       var newBurger = {
-        name: $("#burg").val().trim(),
-        notEaten: $("[name=devoured]:checked").val().trim()
+        burger_name: $("#burg").val().trim(),
+        devoured: $("[name=devoured]:checked").val().trim()
       };
   
       // Send the POST request.
